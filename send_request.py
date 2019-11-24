3a67e16b79c986a83d77c360d75e6249bb1aeace
jenazzad <mansaryouness@gmail.com>
2019-11-24 19:39:22 +0100

    push

--- send_request.py
+++ send_request.py
@@ -8,6 +8,6 @@ headers = {
 
 image_data = open(img_path, "rb").read()
 
-r = requests.post("http://0.0.0.0:5000/predict", headers=headers, data=image_data)
+r = requests.post("http://0.0.0.0:80/predict", headers=headers, data=image_data)
 
 print(r.text)
